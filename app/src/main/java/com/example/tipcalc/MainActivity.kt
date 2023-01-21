0b301083ee18254481b9a12b99aa69313f525638
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2023-01-20 20:17:06 -0600

    made the infoTextView invisible at run time

--- app/src/main/java/com/example/tipcalc/MainActivity.kt
+++ app/src/main/java/com/example/tipcalc/MainActivity.kt
@@ -3,10 +3,16 @@ package com.example.tipcalc
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        infoTextView.visibility = View.INVISIBLE
+
     }
+
 }
